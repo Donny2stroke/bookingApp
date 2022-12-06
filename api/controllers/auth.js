db792fd046781303bc86bc35a8083a4bb77a0e7b
Claudio <claudio2000@hotmail.it>
2022-12-06 13:42:44 +0100

    inserimento Utenti

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,7 +3,9 @@ import User from "../models/User";
 export const register = async (req, res, next) => {
     try {
         const newUser = new User({
-            
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
         })
     } catch (error) {
         next(error);
