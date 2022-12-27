540ab25f6ad1f51fdb94cc0dc124940bad24fd60
Donny2stroke <claudio2000@hotmail.it>
2022-12-27 21:05:13 +0100

    gestione login

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import "./navbar.css"
 import {Link} from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext"
+import { useContext } from "react"
 
 const Navbar = () => {
+
+  const {user} = useContext(AuthContext)
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{color:"inherit", textdecoration:"none"}}>
           <span className="logo">Donny2stroke</span>
           </Link>
-        <div className="navItems">
+        {user ? "ciao "+user.username : (<div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
-        </div>
+        </div>)}
       </div>
     </div>
   )
